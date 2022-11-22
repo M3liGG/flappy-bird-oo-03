3835d4c79eb82f360b99a03a840d259845120629
Melissa <melissa.garcia.garcia@angloaleman.edu.mx>
2022-11-21 18:01:34 -0600

    Registrando eventos

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const juego = {
-    // 1. Agrega la propiedad timerId
-    
-    // 2. Agrega la propiedad gravedad
-    
-  
-    // 3. Agrega el método aleatorio()
+      timerId: 0,
+    graveda: 2,
+
     aleatorio: function (min, max) {
-      // Escribe codigo del método 
+      return Math.floor(Math.random() * (max - min + 1) + min);
     },
   
 
     // 12. Agrega el método loop()
     loop: function () {
      // Escribe codigo del método
+    bird.efectoGravedad();
+    bird.dibujar();
+    juego.verificaColision()
     },
   
     // 13. Agrega el método iniciar()
     iniciar: function() {
-     // Escribe codigo del método
+      document.addEventListener("keyup", bird.mover);
+      juego.timerId = setInterval(juego.loop, 20);
     }
   
   };
@@ -26,33 +27,28 @@ const juego = {
 
 
 const bird = {
-    // 4. Agrega la propiedad div
-  
-
-    // 5. Agrega la propiedad bottom
-   
-
-    // 6. Agrega la propiedad left
+  div: document.querySelector(".bird")
+   bottom: juego.aleatorio(10, 570),
     left: 250,
-
-    // 7. Agrega la propiedad width
     width: 60,
+    height: 45,
 
-    // 8. Agrega la propiedad height
-    
-  
-    // 9. Agrega el método efectoGravedad()
-    efectoGravedad: function () {
-      // Agrega codigo del método
+    efectoGravedad: function (){
+      bird.bottom -= juego.gravedad;
     },
-  
-    // 10. Agrega el método dibujar()
+
     dibujar: function () {
-      //Agrega codigo del método
-    },
+      bird.div.style.bottom = bird.bottom + "px";
+      bird.div.style.left = bird.left + "px";
+    }, 
+
+
+  
+  
 
     // 11. Agrega el método mover()
-
+mover: function(){
+  bird.bottom = 40 + bird.bottom
 }
    
 
